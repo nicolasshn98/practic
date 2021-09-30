64e35b67c2cd0c56cec7a6bef0a45ef0063b3f62
Nicolas <mykolahn@gmail.com>
2021-10-01 00:38:31 +0200

    spread operator/ kloning

--- script.js
+++ script.js
@@ -1,51 +1,49 @@
 'use strict';
 
-const options = {
-	name: 'test',
-	width: 1024,
-	height: 1024,
-	colors: {
-		border: 'black',
-		bg: 'red'
+function copy(mainObj) { // фунція приймає в себе аргумент ( це буде обєкт obj)
+	let objCopy = {}; // пуста перемінна в яку поміститься якби копія obj
+
+	let key; // обявляєм перемінну key
+	for (key in mainObj) { // ключи в mainObj ( якби ключи в obj)
+		objCopy[key] = mainObj[key]; /// в значением ключа в objCopy помещаем значение какби ключів в  obj
+
+	}
+	return objCopy; // наружу
+}
+
+let obj = {
+	a: 2,
+	b: 5,
+	c: {
+		x: 7,
+		y: 4
 	}
 };
 
-for (let key in options) {
-	if (typeof (options[key]) === 'object') {
-		for (let i in options[key]) {
-			console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-		}
-	} else {
-		console.log(`Свойство ${key} имеет значение ${options[key]}`);
-	}
-}
-	
+const newObj = copy(obj); // нова перемінна в якій запускаєм фунціяю яка працює з обєктом obj
+
+newObj.a = 10; // в новій переменні в якій є копія obj в ключа "а" міняємо значення на 10
+
+console.log(newObj);
+console.log(obj);
+
+/// Spread operator
+
+const video = ['youtube', 'vimeo', 'rutube'],
+	blogs = ['wordpress', 'livejournal', 'blogger'],
+	internet = [...video, ...blogs, 'vk', 'facebook'];	// ... витягуэ елементи (Spread operator)
+
+console.log(internet);
 
 
-const information = {
-	name: 'Kolya',
-	subname: 'Hnizdyukh',
-	country: 'Ukraine',
-	age: {
-		number: 16,
-		mounth: 'december',
-		year: 1998
-	}
-};
+// Передаємо елементи з масиву в функціїї
 
-console.log(Object.keys(information).length);
-
-for (let key in information) {
-	if (typeof (information[key]) === 'object') {
-		for (let i in information[key]) {
-			console.log(`Свойство ${i} имеет значение ${information[key][i]}`);
-		}
-	} else {
-		console.log(`Свойство ${key} имеет значение ${information[key]}`);
-	}
+function log(a, b, c) {
+	console.log(a);
+	console.log(b);
+	console.log(c);
 }
 
+const num = [2, 5, 7];
 
-
-
-
+console.log(...num); // розгортає масив і передає в функцію
