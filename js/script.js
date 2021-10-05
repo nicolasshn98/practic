4e2f3c34c746481b9516c8761b7a71af99546867
Nicolas <mykolahn@gmail.com>
2021-10-05 18:20:21 +0200

    lesson 29 elements itd

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
-'use strict';
+// 'use strict';
 
-const box = document.getElementById('box');
+const box = document.getElementById('box'),
+	btns = document.getElementsByTagName('button'),
+	circles = document.getElementsByClassName('circle'),
+	hearts = document.querySelectorAll('.heart'),
+	oneHeart = document.querySelector('.heart'),
+	wrapper = document.querySelector('.wrapper');
 
-console.log(box);
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
 
-const btns = document.getElementsByTagName('button')[1];
+box.style.cssText = 'background-color: blue; width: 500px';
 
-console.log(btns[0]);
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
 
-const circles = document.getElementsByClassName('circle');
-
-console.log(circles[1]);
-
-const hearts = document.querySelectorAll('.heart');
+// for (let i = 0; i < hearts.length; i++) {
+// 	hearts[i].style.backgroundColor = 'blue';
+// }
 
 hearts.forEach(item => {
-	console.log(item);
+	item.style.backgroundColor = 'blue';
 });
 
-const oneHeart = document.querySelector('.heart');
+const div = document.createElement('div');
+// const text = document.createTextNode('hello');
 
-console.log(oneHeart);
+div.classList.add('black');
+
+document.body.append(div);
+
+div.innerHTML = '<h1>Hello World</h1>';
