b87f1e4fd4f34d555a8f55e49c1d52a56baad89c
Nicolas <mykolahn@gmail.com>
2021-10-09 02:14:04 +0200

    practic

--- js/script.js
+++ js/script.js
@@ -1,33 +1,21 @@
-// 'use strict';
+'use strict';
 
-const box = document.getElementById('box'),
-	btns = document.getElementsByTagName('button'),
-	circles = document.getElementsByClassName('circle'),
-	hearts = document.querySelectorAll('.heart'),
-	oneHeart = document.querySelector('.heart'),
-	wrapper = document.querySelector('.wrapper');
+let num = {
+	name: 'Kolya',
+	age: 22,
+	town: 'Ternopil',
+	country: 'Ukraine',
+	phone: 'Inpohe',
+	notebook: 'MacBook Pro'
+};
 
-// box.style.backgroundColor = 'blue';
-// box.style.width = '500px';
 
-box.style.cssText = 'background-color: blue; width: 500px';
 
-btns[1].style.borderRadius = '100%';
-circles[0].style.backgroundColor = 'red';
+for (let i = 0; i < 50; i++) {
 
-// for (let i = 0; i < hearts.length; i++) {
-// 	hearts[i].style.backgroundColor = 'blue';
-// }
+	if (i === 30) {
+		break;
+	}
+	console.log(i);
+}
 
-hearts.forEach(item => {
-	item.style.backgroundColor = 'blue';
-});
-
-const div = document.createElement('div');
-// const text = document.createTextNode('hello');
-
-div.classList.add('black');
-
-document.body.append(div);
-
-div.innerHTML = '<h1>Hello World</h1>';
